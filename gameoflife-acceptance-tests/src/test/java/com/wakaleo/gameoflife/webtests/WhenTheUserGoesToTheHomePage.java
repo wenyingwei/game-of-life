fd2cdd4af9e437adb76dd45108c74fd9291c20bd
Iason-PC\Iason <wenyingwei@cainfoharbor.com>
2017-06-22 16:49:09 +0800

    提交代码

--- gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/WhenTheUserGoesToTheHomePage.java
+++ gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/WhenTheUserGoesToTheHomePage.java
@@ -32,6 +32,6 @@ public class WhenTheUserGoesToTheHomePage {
     public void homePageShouldDisplayWelcomeMessage() {
 
         player.opens_home_page();
-        player.should_see_title_of("The Game Of Life");
+        player.should_see_title_of("The Game Of Life---Iason");
     }
 }
